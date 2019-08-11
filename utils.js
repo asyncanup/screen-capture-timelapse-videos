4a0011f1920b8d1230c06dd5ec29dc99b8c85dd7
Anup Bishnoi <pixelsallover@gmail.com>
2019-08-11 16:51:16 -0700

    now using find instead of ls
    to allow way more files

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ const sh = cmd => execSync(cmd);
 const out = cmd => execSync(cmd).toString().split('\n');
 const l = (...args) => console.log(...args);
 const getImageName = path => path.substr(path.lastIndexOf('/')).substr(1).split('.png')[0].replace(/\./g, ':').replace('T', ' ');
-const getImagePathsForDay = day => out(`ls screenshots/*/${day}*.png`);
+const getImagePathsForDay = day => out(`find screenshots/ -name '${day}*.png'`);
 const getImageSignature = path => out(`magick identify -verbose '${path}' | grep signature`)[0].trim().split(' ')[1];
 const removeImagesForDay = day => sh(`rm screenshots/*/${day}*.png`);
 
